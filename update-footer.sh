9d88f2ee80c9f4fcebd9b7988cd719e153e75423
Matt Lease <ml@ischool.utexas.edu>
2016-08-05 19:14:42 +0000

    remove platinum sponsor until we have one!

--- update-footer.sh
+++ update-footer.sh
@@ -3,7 +3,7 @@
 mkdir -p tmp
 for f in *.html; do
 	g=tmp/"$f";
-	grep -B1000 Footer "$f" | sed '$d' > "$g";
+	grep -B10000 Footer "$f" | sed '$d' > "$g";
 	cat footer.htm >> "$g";
 	mv "$g" "$f";
 done
